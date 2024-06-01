13584ecca94f4d3c17cc9ba091e59257e5895907
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 18:52:59 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Aplicaciones de Inteligencia Artificial")
 st.subheader("Conversión de texto a voz")
 image = Image.open('txt_to_audio.png')
-st.image(image, width=400)
+st.image(image, width=250)
 st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
 texto_del_enlace = "Texto a voz"
 url = "https://imultimod.streamlit.app/"
