65de39d240ae8b8378ce5237f50ae53c1898986c
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 18:38:41 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("Aplicaciones de Inteligencia Artificial")
 
 st.subheader("Conversión de texto a voz")
+st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
 texto_del_enlace = "Texto a voz"
 url = "https://imultimod.streamlit.app/"
 st.write(f"Texto a voz: [Enlace]({url})")
