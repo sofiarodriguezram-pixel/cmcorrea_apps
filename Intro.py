80c9393606a8614e10ed5ce04f8f6312f54b263f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 18:26:11 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -5,5 +5,5 @@ st.title("Aplicaciones de Inteligencia Artificial")
 st.subheader("Conversión de texto a voz")
 texto_del_enlace = "Texto a voz"
 url = "https://imultimod.streamlit.app/"
-st.write(f"check out this [link]({url})")
+st.write(f"Texto a voz: [Enlace]({url})")
 st.markdown(f"{texto_del_enlace}")
