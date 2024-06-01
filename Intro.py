074f3cec2eee2114e2866c8ed2e71ed0ec3708db
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 18:19:52 -0500

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Aplicaciones de Inteligencia Artificial")
+
+st.subheader("Conversión de texto a voz")
+texto_del_enlace = "Texto a voz"
+url = "https://imultimod.streamlit.app/"
+st.markdown(f"{texto_del_enlace}")
