a1b112897d2d03e8063e2eb7b0dbd64807ce7a03
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 19:19:18 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -7,15 +7,15 @@ with col1:
  st.title("Aplicaciones de Inteligencia Artificial.")
  st.subheader("Conversión de texto a voz")
  image = Image.open('txt_to_audio.png')
- st.image(image, width=300)
+ st.image(image, width=200)
  st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
  url = "https://imultimod.streamlit.app/"
  st.write(f"Texto a voz: [Enlace]({url})")
 
-with col1: 
+with col2: 
  st.subheader("Conversión de voz a texto")
  image = Image.open('audio_to_txt.png')
- st.image(image, width=300)
+ st.image(image, width=200)
  st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
  url = "https://traductor-ab0sp9f6fi.streamlit.app/"
  st.write(f"Voz a texto: [Enlace]({url})")
