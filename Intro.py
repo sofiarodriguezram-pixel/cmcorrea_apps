a91f7c82e2ee228080f5fb9329a968b593359abb
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-01 19:16:51 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,17 +1,20 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Aplicaciones de Inteligencia Artificial.")
-st.subheader("Conversión de texto a voz")
-image = Image.open('txt_to_audio.png')
-st.image(image, width=300)
-st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
-url = "https://imultimod.streamlit.app/"
-st.write(f"Texto a voz: [Enlace]({url})")
+col1, col2, col3 = st.columns(3)
 
-st.subheader("Conversión de voz a texto")
-image = Image.open('audio_to_txt.png')
-st.image(image, width=300)
-st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
-url = "https://traductor-ab0sp9f6fi.streamlit.app/"
-st.write(f"Voz a texto: [Enlace]({url})")
+with col1:
+ st.title("Aplicaciones de Inteligencia Artificial.")
+ st.subheader("Conversión de texto a voz")
+ image = Image.open('txt_to_audio.png')
+ st.image(image, width=300)
+ st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
+ url = "https://imultimod.streamlit.app/"
+ st.write(f"Texto a voz: [Enlace]({url})")
+
+ st.subheader("Conversión de voz a texto")
+ image = Image.open('audio_to_txt.png')
+ st.image(image, width=300)
+ st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
+ url = "https://traductor-ab0sp9f6fi.streamlit.app/"
+ st.write(f"Voz a texto: [Enlace]({url})")
