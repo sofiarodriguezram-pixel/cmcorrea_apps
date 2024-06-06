9523b10e205576df3baff77c52a2e800d8e1ac59
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-06 10:00:34 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -12,8 +12,8 @@ with st.sidebar:
   st.write(parrafo)
 
 url_ia="https://sites.google.com/view/aplicacionesdeia/inicio"
-st.write("En la siguiente enlace puedes encontrar páginas y ejercicios prácticos")
-st.write(f"Texto a voz: [Enlace]({url_ia})")
+st.subheader("En la siguiente enlace puedes encontrar páginas y ejercicios prácticos")
+st.write(f"Enlace para páginas y ejercicios: [Enlace]({url_ia})")
 col1, col2, col3 = st.columns(3)
 
 with col1:
